4389059ccf766ff98f0740ecfa2f0f21fe87b6ee
Pritee Kumari <123693971+TonyMartin1102@users.noreply.github.com>
2023-10-19 09:36:36 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,10 +4,6 @@ import os
 import asyncio
 import traceback
 
-sudo apt-get install gcc libpq-dev -y
-sudo apt-get install python-dev  python-pip -y
-sudo apt-get install python3-dev python3-pip python3-venv python3-wheel -y
-pip3 install wheel
 
 lazy_pic = os.environ.get("LAZY_PIC","")
 from binascii import (
